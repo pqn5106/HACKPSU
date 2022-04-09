46a53bd1a8c80cc7a237fcc280bcef1b0844b418
Param Nigam <pqn5106@psu.edu>
2022-04-09 17:23:05 -0400

    Initial Commit

---
+++ HACKPSU/HACKPSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HACKPSUApp.swift
+//  HACKPSU
+//
+//  Created by Param Nigam on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HACKPSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
